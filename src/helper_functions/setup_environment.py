b9fe1aeaa2322a640251d8a5adf54d3b21690c34
michaelgorkow <michelgorkow@gmail.com>
2025-02-10 03:14:18 +0100

    32

--- src/helper_functions/setup_environment.py
+++ src/helper_functions/setup_environment.py
@@ -11,5 +11,5 @@ def setup_demo(session: Session):
     root.databases[database].schemas.create(schema=Schema(name="FEATURE_STORE"), mode=CreateMode.or_replace)
     root.databases[database].schemas.create(schema=Schema(name="MODEL_REGISTRY"), mode=CreateMode.or_replace)
     session.table(f'{database}._DATA_GENERATION._TRANSACTIONS').filter(col('DATE') <= lit('2024-04-30')).write.save_as_table(table_name=f'{database}.RETAIL_DATA.TRANSACTIONS', mode='overwrite')
-    session.table(f'{database}._DATA_GENERATION._CUSTOMERS').write.save_as_table('SIMPLE_MLOPS_DEMO.RETAIL_DATA.CUSTOMERS')
+    session.table(f'{database}._DATA_GENERATION._CUSTOMERS').write.save_as_table(f'{database}.RETAIL_DATA.CUSTOMERS')
     print('Setup finished.')
